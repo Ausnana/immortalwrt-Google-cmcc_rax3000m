299145b88d24557c5881fa3259f5dc91703afebb
Ausnana <120234290+Ausnana@users.noreply.github.com>
2025-12-08 01:55:30 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,41 @@
+
 #!/bin/bash
 #
+# 版权所有 (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# 这是一个自由软件，根据 MIT 许可证授权。
+# 详细信息请参见 /LICENSE。
+#
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认 IP 为 192.168.6.1
+CONFIG_FILE="package/base-files/files/bin/config_generate"
+if [ -f "$CONFIG_FILE" ]; then
+  sed -i 's/192\.168\.1\.1/192.168.6.1/g' "$CONFIG_FILE"
+  echo "IP 地址已更新为 192.168.6.1"
+else
+  echo "警告：$CONFIG_FILE 不存在，跳过 IP 修改"
+fi
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 预装 OpenClash（已注释，保持不变）
+# echo "CONFIG_PACKAGE_luci-app-openclash=y" >> .config
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
+rm -f package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p
+if [ $? -eq 0 ]; then
+  echo "已删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p"
+else
+  echo "错误：删除 package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/e2p 失败"
+fi
+
+# 创建 MT7981 固件符号链接
+EEPROM_FILE="package/mtk/drivers/mt_wifi/files/mt7981-default-eeprom/MT7981_iPAiLNA_EEPROM.bin"
+if [ -f "$EEPROM_FILE" ]; then
+  mkdir -p files/lib/firmware
+  ln -sf /lib/firmware/MT7981_iPAiLNA_EEPROM.bin files/lib/firmware/e2p
+  echo "符号链接已创建"
+  ls -l files/lib/firmware/e2p || { echo "错误：符号链接创建失败"; exit 1; }
+else
+  echo "错误：$EEPROM_FILE 不存在，无法创建符号链接"
+  exit 1
+fi
